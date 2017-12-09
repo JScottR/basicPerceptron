e2b188dde48ab91d11f4434418b3aa445f08d5b4
JScott <JScott@JSCOTT-PC>
2017-12-09 15:54:55 -0700

    numbers become more stable when cleaning up data after sig function

--- basicPerceptron/basicPerceptron.py
+++ basicPerceptron/basicPerceptron.py
@@ -1,7 +1,7 @@
 import random
 import math
 
-alpha = 0.5
+alpha = 0.1
 
 def randomValue():
     return math.floor(random.random()*2) 
@@ -43,14 +43,18 @@ def main(nodeA,nodeB,nodeC,nodeD):
         else:
             expectedOutput = 0
         sumInputs = nodeA.weight*nodeA.value + nodeB.weight*nodeB.value + nodeC.weight*nodeC.value + nodeD.weight*nodeD.value
-        output = 1/(1+math.exp(-1*sumInputs))
+        sigOutput = 1/(1+math.exp(-1*sumInputs))
+        if sigOutput >= 0.5:
+            output = 1
+        else:
+            output = 0
         if i % 250 == 0:
             printNodes(nodeA,nodeB,nodeC,nodeD)
             print(output)
         if(output != expectedOutput):
-            nodeA.weight += (alpha * (expectedOutput - output) * (output*(1-output)) * nodeA.value)
-            nodeB.weight += (alpha * (expectedOutput - output) * (output*(1-output)) * nodeB.value)
-            nodeC.weight += (alpha * (expectedOutput - output) * (output*(1-output)) * nodeC.value)
+            nodeA.weight += (alpha * (expectedOutput - output) * (sigOutput*(1-sigOutput)) * nodeA.value)
+            nodeB.weight += (alpha * (expectedOutput - output) * (sigOutput*(1-sigOutput)) * nodeB.value)
+            nodeC.weight += (alpha * (expectedOutput - output) * (sigOutput*(1-sigOutput)) * nodeC.value)
         newValues(nodeA,nodeB,nodeC)
 
 
