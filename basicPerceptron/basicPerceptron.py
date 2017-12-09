57f813d83e10f159d9be01eb5aede524ace60747
JScott <JScott@JSCOTT-PC>
2017-12-09 13:45:41 -0700

    some code fixes to make cleaner basic bones of project

--- basicPerceptron/basicPerceptron.py
+++ basicPerceptron/basicPerceptron.py
@@ -1,6 +1,8 @@
 import random
 import math
 
+alpha = 0.3
+
 def randomValue():
     return math.floor(random.random()*2) 
 
@@ -8,38 +10,55 @@ def randomWeight():
     return random.uniform(-1,1)
 
 class inputNodes:
-    # should be either 0 or 1 
-    value = 1
-    ## weight value
-    weight = -1
 
-def init(nodeA,nodeB,nodeC):
-    nodeA.value = randomValue()
-    nodeA.weight = randomWeight()
-    nodeB.value =  randomValue()
-    nodeB.weight = randomWeight()
-    nodeC.value =  randomValue()
-    nodeC.weight = randomWeight()
-
+    def __init__(self, value=1, weight= -1, name="node"):
+        self.value = value
+        self.weight = weight
+        self.name = name
+    def nodePrint(self):
+        output = 'The value of ' + repr(self.name) + ' is ' + repr(self.value) + ' and the weight is ' + repr(self.weight)
+        print(output)
+    def weightPrint(self):
+        output = 'The weight of ' + repr(self.name) + ' is ' +  repr(self.weight)
+        print(output)
+    def valuePrint(self):
+        output = 'The value of ' + repr(self.name) + ' is ' +  repr(self.value)
+        print(output)
+        
 def printNodes(nodeA,nodeB,nodeC,nodeD):
-    print(nodeA.value)
-    print(nodeA.weight)
-    print(nodeB.value)
-    print(nodeB.weight)
-    print(nodeC.value)
-    print(nodeC.weight)
-    print(nodeD.value)
-    print(nodeD.weight)
+    nodeA.nodePrint()
+    nodeB.nodePrint()
+    nodeC.nodePrint()
+    nodeD.nodePrint()
 
 def newValues(nodeA,nodeB,nodeC):
     nodeA.value = randomValue()
     nodeB.value = randomValue()
     nodeC.value = randomValue()
 
-nodeA = inputNodes()
-nodeB = inputNodes()
-nodeC = inputNodes()
-nodeD = inputNodes()
+def main(nodeA,nodeB,nodeC,nodeD):
+    for i in range(0,8001):
+        if nodeA.value == 1 and nodeC.value == 1:
+            expectedOutput = 1
+        else:
+            expectedOutput = 0
+        sumInputs = nodeA.weight*nodeA.value + nodeB.weight*nodeB.value + nodeC.weight*nodeC.value + nodeD.weight*nodeD.value
+        output= 1/(1+math.exp(-sumInputs))
+        if i % 250 == 0:
+            printNodes(nodeA,nodeB,nodeC,nodeD)
+            print(output)
+        if(output != expectedOutput):
+            nodeA.weight += (alpha * (expectedOutput - output) * (output*(1-output)) * nodeA.value)
+            nodeB.weight += (alpha * (expectedOutput - output) * (output*(1-output)) * nodeB.value)
+            nodeC.weight += (alpha * (expectedOutput - output) * (output*(1-output)) * nodeC.value)
+        newValues(nodeA,nodeB,nodeC)
 
-init(nodeA,nodeB,nodeC)
-printNodes(nodeA,nodeB,nodeC,nodeD)
+
+nodeA = inputNodes(randomValue(),randomWeight(),"nodeA")
+nodeB = inputNodes(randomValue(),randomWeight(),"nodeB")
+nodeC = inputNodes(randomValue(),randomWeight(),"nodeC")
+nodeD = inputNodes(1,-1,"nodeD")
+
+main(nodeA,nodeB,nodeC,nodeD)
+
+
